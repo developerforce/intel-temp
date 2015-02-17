2b31ad3054cc2d160f6d720cdbd86f446095541c
James Ward <james@jamesward.org>
2015-02-17 16:18:28 -0700

    add some logging

--- app/jobs/Tick.scala
+++ app/jobs/Tick.scala
@@ -3,9 +3,10 @@ package jobs
 import java.io.File
 
 import play.api.libs.concurrent.Akka
-import play.api.{Play, Mode, DefaultApplication}
+import play.api.{Logger, Play, Mode, DefaultApplication}
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
 
 object Tick extends App {
 
@@ -16,7 +17,12 @@ object Tick extends App {
   val main = Main(app)
 
   Akka.system.scheduler.schedule(Duration.Zero, 20.seconds) {
-    main.run
+    main.run.onComplete {
+      case Success(s) =>
+        Logger.info(s.toString)
+      case Failure(e) =>
+        Logger.error(e.getMessage)
+    }
   }
 
 }
